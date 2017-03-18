89039896ce2d5c55258580d10bf3939f72133399
Junqing Qiao <junqingqiao@gmail.com>
2017-03-17 22:54:55 -0400

    Added build file

---
+++ Build.R
@@ -0,0 +1,4 @@
+library(knitr)
+library(ggplot2)
+knit('C:/Users/Junqing/Documents/College/Junior Spring/Stats 525/IE/IE.Rmd')
+render("C:/Users/Junqing/Documents/College/Junior Spring/Stats 525/IE/IE.md", "IE.pdf")
